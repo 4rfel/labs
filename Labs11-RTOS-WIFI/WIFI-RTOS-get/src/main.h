f54c70a9087710223e112448205d42cf0cac4c20
Arfel <rafael_1999_@hotmail.com.br>
2020-05-29 17:35:15 -0300

    aa

--- Labs11-RTOS-WIFI/WIFI-RTOS-get/src/main.h
+++ Labs11-RTOS-WIFI/WIFI-RTOS-get/src/main.h
@@ -50,7 +50,7 @@ extern "C" {
 /** Wi-Fi Settings */
 #define MAIN_WLAN_SSID                    "Casa" /**< Destination SSID */
 #define MAIN_WLAN_AUTH                    M2M_WIFI_SEC_WPA_PSK /**< Security manner */
-#define MAIN_WLAN_PSK                     "a1b2c3d4e9" /**< Password for Destination SSID */
+#define MAIN_WLAN_PSK                     "n vou upar minha senha no git" /**< Password for Destination SSID */
 
 /** Using broadcast address for simplicity. */
 #define MAIN_SERVER_PORT                    (5000)
